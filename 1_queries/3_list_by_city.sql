cd0a42aed101d0b25de1d0fd712bc3d19be07668
Teenaelza <teena.thaliath@gmail.com>
2021-09-16 04:29:25 +0000

    IMPLEMENT LighthouseBnB select queries

---
+++ 1_queries/3_list_by_city.sql
@@ -0,0 +1,6 @@
+SELECT properties.*,AVG(property_reviews.rating) AS average_rating
+FROM properties 
+JOIN property_reviews ON property_id=properties.id
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating)>=4
+ORDER BY cost_per_night limit 10;
