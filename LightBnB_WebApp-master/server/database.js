547b96b390770968e46227da9e24382b56329bf6
Teenaelza <teena.thaliath@gmail.com>
2021-09-16 22:41:52 +0000

    Implement LighthouseBnB Reservations challenge

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,7 +85,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.*,properties.*,AVG(property_reviews.rating) AS average_rating
+      FROM properties 
+      JOIN reservations ON reservations.property_id=properties.id
+      JOIN property_reviews ON property_reviews.reservation_id=reservations.id
+      WHERE reservations.guest_id = $1 
+      AND reservations.end_date < now()::date
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date 
+      LIMIT $2`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      return err.message;
+    });
 };
 exports.getAllReservations = getAllReservations;
 
