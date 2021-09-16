f80a81536d6910b4508b1252fb6ede19fa7d2540
Teenaelza <teena.thaliath@gmail.com>
2021-09-16 03:30:09 +0000

    Add seed file for our LightBnB database

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,46 @@
+
+INSERT INTO users (name,email,password) VALUES ('amessingham0','mdelicate0@multiply.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('gomurtagh1','chackwell1@cam.ac.uk','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('clicquorish2','bpinar2@shinystat.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('kfrapwell3','jdeclerq3@cisco.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('arastrick4','kbaverstock4@miitbeian.gov.cn','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('jgotthard5','mscotts5@upenn.edu','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('dbullcock6','rlawrey6@squarespace.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('rclaesens7','jfentem7@smugmug.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('tdibatista8','mhonnicott8@ocn.ne.jp','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name,email,password) VALUES ('lidale9','ekerford9@usa.gov','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+ALTER SEQUENCE users_id_seq RESTART WITH 11;
+
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (1,'Speedlamp','description','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',930.61,6,4,8,'Canada','536NamsubHighway','Sotboske','Quebec','28142','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (1,'Blankcorner','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',85234,6,6,7,'Canada','651NamiRoad','Bohbatev','Alberta','83680','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (2,'Habitmix','description','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',46058,0,5,6,'Canada','1650HejtoCenter','Genwezuj','NewfoundlandAndLabrador','44583','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (4,'Headedknow','description','https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg',2640,0,5,5,'Canada','513PowovGrove','Jaebvap','Ontario','38051','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (6,'Portout','description','https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg',2358,2,8,0,'Canada','1392GazaJunction','Upetafpuv','NovaScotia','81059','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (6,'Funglad','description','https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg',34291,6,6,4,'Canada','169NuwugCircle','Vutgapha','NewfoundlandAndLabrador','00159','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (7,'Shinetwenty','description','https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg',13644,1,7,8,'Canada','340DoktoPark','Upfufa','NovaScotia','29045','true');
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms, number_of_bedrooms,country,street,city,province,post_code,active) VALUES (8,'Gamefill','description','https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg',23428,5,6,4,'Canada','834BuwmiRoad','Rotunif','NewfoundlandAndLabrador','58224','true');
+
+ALTER SEQUENCE properties_id_seq RESTART WITH 9;
+
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2018-09-11','2018-09-26',2,3);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2019-01-04','2019-02-01',2,2);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2021-10-01','2021-10-14',1,4);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2014-10-21','2014-10-21',3,5);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2016-07-17','2016-08-01',3,4);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2018-05-01','2018-05-27',4,8);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2022-10-04','2022-10-23',5,1);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2015-09-13','2015-09-30',6,8);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2023-05-27','2023-05-28',4,2);
+INSERT INTO reservations (start_date,end_date,property_id,guest_id) VALUES ('2023-04-23','2023-05-02',8,1);
+ALTER SEQUENCE reservations_id_seq RESTART WITH 11;
+
+
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (2,5,10,3,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (1,4,1,4,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (8,1,2,4,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (3,8,5,4,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (4,2,7,5,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (4,3,4,4,'messages');
+INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message) VALUES (5,6,3,5,'messages');
+ALTER SEQUENCE property_reviews_id_seq RESTART WITH 8;
